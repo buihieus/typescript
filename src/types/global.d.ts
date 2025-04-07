dc298ff3950aee0466780adf50b2f3fd5c8fc434
Bui Cong Hieu <buihieu9259@gmail.com>
2025-04-07 23:18:30 +0700

    first commit

---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
+
+// dùng để khi chuyển từ js sang ts thì các phần đường dẫn ảnh ko bị lỗi
